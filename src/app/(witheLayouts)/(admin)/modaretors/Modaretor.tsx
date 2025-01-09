c68b60912b0e663cf4b3088b50c61ba1bceac47d
Suhnaur-Rohoman-Sumon-github <sumonsuhanurrohoman@gmail.com>
2025-01-09 18:27:18 +0600

    somem ore changes for solving eror

---
+++ src/app/(witheLayouts)/(admin)/modaretors/Modaretor.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Modaretor = () => {
+    return (
+        <div>
+            <h1>this is modaretro</h1>
+        </div>
+    );
+};
+
+export default Modaretor;
